b4460584da50fd80d6c398c2f1aee223cd780aa9
RosGoodman <pavel.official@bk.ru>
2022-02-01 17:09:53 +0300

    основа

---
+++ AutoMapper/AutoMapper/DAL/Context/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoMapper.DAL.Context
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConString"));
+        }
+    }
+}
